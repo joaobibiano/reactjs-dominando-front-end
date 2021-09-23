697b44acb94fbff51c0199c1bd169854af12eeb3
João Victor - Macbook <jv.bibiano@hotmail.com>
2021-09-23 20:38:25 +0100

    build Row, Box, FuelText and FuelPrice component

--- 025-fuelprice-project/src/components/Fuel/index.tsx
+++ 025-fuelprice-project/src/components/Fuel/index.tsx
@@ -1,11 +1,49 @@
 import React from "react";
-import { Container, SettingsIcon, Title } from "./styles";
+import {
+  Box,
+  Container,
+  FuelPrice,
+  FuelText,
+  Panel,
+  Row,
+  SettingsIcon,
+  Title,
+} from "./styles";
 
 export const FuelComponent = () => {
   return (
     <Container>
       <Title>Posto ReactJS</Title>
       <SettingsIcon />
+
+      <Panel>
+        <Row>
+          <Box>
+            <FuelText>Gasolina Comum</FuelText>
+          </Box>
+          <Box>
+            <FuelPrice>4.59</FuelPrice>
+          </Box>
+        </Row>
+
+        <Row>
+          <Box>
+            <FuelText>Gasolina Comum</FuelText>
+          </Box>
+          <Box>
+            <FuelPrice>4.59</FuelPrice>
+          </Box>
+        </Row>
+
+        <Row>
+          <Box>
+            <FuelText>Gasolina Comum</FuelText>
+          </Box>
+          <Box>
+            <FuelPrice>4.59</FuelPrice>
+          </Box>
+        </Row>
+      </Panel>
     </Container>
   );
 };
--- 025-fuelprice-project/src/components/Fuel/styles.ts
+++ 025-fuelprice-project/src/components/Fuel/styles.ts
@@ -34,3 +34,52 @@ export const SettingsIcon = styled(FiSettings)`
   top: 20px;
   right: 20px;
 `;
+
+export const Panel = styled.section`
+  width: 300px;
+  height: 500px;
+  background-color: ${(props) => props.theme.colorGrey100};
+
+  border-radius: 5px;
+
+  position: relative;
+  left: calc(100vw - 350px);
+  margin-top: 60px;
+`;
+
+export const Box = styled.div`
+  width: 135px;
+  height: 65px;
+
+  background-color: ${(props) => props.theme.colorBlue200};
+
+  border-radius: 5px;
+
+  display: flex;
+  justify-content: center;
+  align-items: center;
+`;
+
+export const Row = styled.div`
+  display: flex;
+
+  justify-content: space-around;
+
+  padding-top: 15px;
+`;
+
+export const FuelText = styled.span`
+  color: ${(props) => props.theme.colorWhite};
+  font-size: 1.5rem;
+  font-weight: bold;
+`;
+
+export const FuelPrice = styled.span`
+  font-family: "Digital-7 Mono", sans-serif;
+
+  color: ${(props) => props.theme.colorYellow600};
+
+  font-size: 3.5rem;
+
+  text-align: left;
+`;
