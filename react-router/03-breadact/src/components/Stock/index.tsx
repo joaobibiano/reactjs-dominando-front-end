f59fd015229fe11c8ae077e4133910fc948f2e7d
João Victor - Macbook <jv.bibiano@hotmail.com>
2021-10-02 00:41:05 +0100

    useLocation and queryParam

--- react-router/03-breadact/src/components/Stock/index.tsx
+++ react-router/03-breadact/src/components/Stock/index.tsx
@@ -2,7 +2,7 @@ import {
   useParams,
   useHistory,
   useRouteMatch,
-  generatePath,
+  useLocation,
 } from "react-router-dom";
 import data from "../../assets/data.json";
 import { ButtonVisibily, StockContainer } from "./styles";
@@ -11,13 +11,23 @@ import { AiFillEyeInvisible, AiFillEye } from "react-icons/ai";
 export const Stock = () => {
   const history = useHistory();
   const routeMatch = useRouteMatch();
-  const showStock = false;
+  const location = useLocation();
+  const queryParam = new URLSearchParams(location.search);
+
+  const showStock = queryParam.get("showStock") === "true";
+
+  function toggleShow() {
+    history.replace({
+      pathname: routeMatch.path,
+      search: `?showStock=${!showStock}`,
+    });
+  }
 
   return (
     <div>
       <h3>
         Estoque de produtos
-        <ButtonVisibily>
+        <ButtonVisibily onClick={toggleShow}>
           {showStock ? <AiFillEyeInvisible /> : <AiFillEye />}
         </ButtonVisibily>
       </h3>
