790989c15c3be58e77937be34353dcde6bc6524e
João Victor - Macbook <jv.bibiano@hotmail.com>
2021-10-01 23:07:20 +0100

    implement redirect

--- react-router/03-breadact/src/App.tsx
+++ react-router/03-breadact/src/App.tsx
@@ -1,13 +1,17 @@
 import { useState } from "react";
 import logo from "./logo.svg";
 import "./App.css";
-import { BrowserRouter, Switch, Route } from "react-router-dom";
+import { BrowserRouter, Switch, Route, Redirect } from "react-router-dom";
 
 function App() {
   return (
     <div className="App">
       <BrowserRouter>
         <Switch>
+          <Route exact path="/">
+            <Redirect to="/production" />
+          </Route>
+
           <Route path="/production">Produção</Route>
 
           <Route path="/stock">Estoque</Route>
