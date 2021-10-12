763a5bf7b178611e6e07ceabb370632957fb7637
João Victor - Macbook <jv.bibiano@hotmail.com>
2021-10-12 21:51:41 +0100

    controlled component

--- forms/02-forms-intro/src/App.tsx
+++ forms/02-forms-intro/src/App.tsx
@@ -24,4 +24,44 @@ function Form_Uncontrolled() {
   );
 }
 
-export default Form_Uncontrolled;
+function Form_controlled() {
+  const [name, setName] = useState("");
+  const [email, setEmail] = useState("");
+
+  // const [data, setData] = useState({
+  //   name: '',
+  //   email: ''
+  // })
+
+  return (
+    <div className="App">
+      <form
+        onSubmit={(event) => {
+          event.preventDefault();
+          console.log(name, email);
+        }}
+      >
+        <label htmlFor="name">Nome</label>
+        <input
+          type="text"
+          name="name"
+          placeholder="Digite seu nome"
+          value={name}
+          onChange={(event) => setName(event.target.value)}
+        />
+        {name === "joao" && <h1>Seja bem vindo!</h1>}
+
+        <input
+          type="text"
+          name="email"
+          placeholder="Digite seu e-mail"
+          value={email}
+          onChange={(event) => setEmail(event.target.value)}
+        />
+        <input type="submit" value="Enviar"></input>
+      </form>
+    </div>
+  );
+}
+
+export default Form_controlled;
