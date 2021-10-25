e5dd04ebb868c1eff6315529d4f8316334f6103c
João Victor - Macbook <jv.bibiano@hotmail.com>
2021-10-25 07:47:04 +0100

    add App test

--- testing/project/src/App.test.tsx
+++ testing/project/src/App.test.tsx
@@ -1,9 +1,9 @@
-import React from 'react';
-import { render, screen } from '@testing-library/react';
-import App from './App';
+import React from "react";
+import { render, screen } from "@testing-library/react";
+import App from "./App";
 
-test('renders learn react link', () => {
+it("renders Todo Component", () => {
   render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+
+  expect(screen.getByPlaceholderText("Type your todo")).toBeInTheDocument();
 });
