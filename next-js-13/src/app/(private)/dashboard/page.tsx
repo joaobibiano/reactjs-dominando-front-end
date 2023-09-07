d0813486658164055d7cdb2853f4292d64dadd18
João Bibiano <jv.bibiano@hotmail.com>
2023-09-07 12:27:03 -0300

    private layout

--- next-js-13/src/app/(private)/dashboard/page.tsx
+++ next-js-13/src/app/(private)/dashboard/page.tsx
@@ -1,5 +1,18 @@
+import { Button } from "@/components/ui/button";
+import { H3 } from "@/components/ui/typography";
 import Image from "next/image";
+import Link from "next/link";
 
 export default function Dashboard() {
-  return <p>Olá do Dashboard</p>;
+  return (
+    <>
+      <section className="flex justify-between">
+        <H3>Seus links</H3>
+
+        <Link href="/dashboard/new">
+          <Button className="bg-indigo-600">Adicionar</Button>
+        </Link>
+      </section>
+    </>
+  );
 }
--- next-js-13/src/app/(private)/layout.tsx
+++ next-js-13/src/app/(private)/layout.tsx
@@ -1,3 +1,4 @@
+import { H1 } from "@/components/ui/typography";
 import { UserButton } from "@clerk/nextjs";
 export default function RootLayout({
   children,
@@ -5,9 +6,18 @@ export default function RootLayout({
   children: React.ReactNode;
 }) {
   return (
-    <main>
-      <UserButton />
-      {children}
-    </main>
+    <>
+      <nav className="flex justify-between mx-24 mt-6">
+        <H1>NextShort</H1>
+
+        <div>
+          <UserButton afterSignOutUrl="/" />
+        </div>
+      </nav>
+
+      <section className="rounded-lg shadow-2xl px-24 py-12 mx-3 my-12 h-3/5">
+        {children}
+      </section>
+    </>
   );
 }
